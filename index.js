eeb7dd252324b28922c7ef5f744c99b19ad578b4
Perduracion <perduracion@gmail.com>
2024-05-07 18:19:57 -0300

    Actualización de arquitectura y diseño
    Framework: Vue.js y Bootstrap
    Scrum es un marco de trabajo para desarrollo ágil de software que se ha expandido a otras industrias.
    
    Co-Authored-By: Hernan Merino <97014348+Hermerino@users.noreply.github.com>
    Co-Authored-By: gustavo <67236116+gustaboin@users.noreply.github.com>
    Co-Authored-By: epagnone <17068830+epagnone@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,16 +1,45 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function ()
+{
     const formulario = document.querySelector('form');
 
-    formulario.addEventListener('submit', function(evento) {
+    formulario.addEventListener('submit', function (evento)
+    {
         const nombre = document.getElementById('nombre').value.trim();
         const apellido = document.getElementById('apellido').value.trim();
         const email = document.getElementById('email').value.trim();
         const comentario = document.getElementById('comentario').value.trim();
         const direccion = document.getElementById('direccion').value.trim();
 
-        if (!nombre || !apellido || !email || !comentario) {
+        if (!nombre || !apellido || !email || !comentario)
+        {
             evento.preventDefault();
             alert('Uno o más campos requeridos no fueron completados. Complételos antes de enviar el formulario');
         }
     });
 });
+
+
+
+/** js para menu responsive al usar la web en celulares */
+
+let showMenu = false;
+
+function toggleMenu()
+{
+    if (showMenu)
+    {
+        document.getElementById("nav").classList = "";
+        showMenu = false;
+    } else
+    {
+        document.getElementById("nav").classList = "responsive";
+        showMenu = true;
+    }
+}
+
+
+function closeMenu()
+{
+    document.getElementById("nav").classList = "";
+    showMenu = false;
+}
