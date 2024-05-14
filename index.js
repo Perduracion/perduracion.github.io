f3de249c66d67f38d5dd9e6af296ba8b390505c3
Perduracion <perduracion@gmail.com>
2024-05-14 05:31:54 -0300

    Vuejs y Web3Form modified:   Contacto.html modified:   index.js

--- index.js
+++ index.js
@@ -14,3 +14,39 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+
+<script setup lang="ts">
+import { ref } from "vue";
+const WEB3FORMS_ACCESS_KEY = "YOUR_ACCESS_KEY_HERE";
+const name = ref("")
+const email = ref("")
+const message = ref("")
+
+const submitForm = async () => {
+const response = await fetch("https://api.web3forms.com/submit", {
+    method: "POST",
+    headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+    },
+    body: JSON.stringify({
+    access_key: WEB3FORMS_ACCESS_KEY,
+    name: name.value,
+    email: email.value,
+    message: message.value,
+    }),
+});
+const result = await response.json();
+if (result.success) {
+    console.log(result);
+}
+}
+</script>
+<template>
+<form @submit.prevent="submitForm">
+    <input type="text" name="name" v-model="name"/>
+    <input type="email" name="email"  v-model="email"/> 
+    <textarea name="message" v-model="message"></textarea>
+    <button type="submit">Send Message</button>
+</form>
+</template>
