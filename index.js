1f32647cfc36c80619020f6a48bf18770c1022f2
Perduracion <perduracion@gmail.com>
2024-05-14 05:43:12 -0300

    Me canse de git modified:   Contacto.html modified:   index.js

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ const form = document.getElementById('form');
 const result = document.getElementById('result');
 
 form.addEventListener('submit', function(e) {
-  e.preventDefault();
-  const formData = new FormData(form);
-  const object = Object.fromEntries(formData);
-  const json = JSON.stringify(object);
-  result.innerHTML = "Please wait..."
+e.preventDefault();
+const formData = new FormData(form);
+const object = Object.fromEntries(formData);
+const json = JSON.stringify(object);
+result.innerHTML = "Please wait..."
 
     fetch('https://api.web3forms.com/submit', {
             method: 'POST',
